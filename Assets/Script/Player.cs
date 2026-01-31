e89f1bc680fb753e563b457f6d1fc658ef551fab
rdelpy <romain.delpy@u-bordeaux.fr>
2026-01-31 13:01:54 +0100

    New Wall Mech & Added complex to detection

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,7 @@ public class Player : MonoBehaviour
     /// The time of the dash
     /// </summary>
     [SerializeField] private float dashSpeed;
+    private Tween lastMove;
     /// <summary>
     /// The player's collider, needed to get its dimension
     /// </summary>
@@ -30,34 +32,67 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            Mouse mouse = Mouse.current;
-            if(mouse.leftButton.wasPressedThisFrame)
-                MoveToPos(GetDestPos());
+        Mouse mouse = Mouse.current;
+        if(mouse.leftButton.wasPressedThisFrame){
+            lastMove.Kill();
+            float time;
+            Vector2 dest = GetDestPos(out time);
+            MoveToPos(dest,time);
+        }
+    }
+
+    /// <summary>
+    /// Will check when the player hits something
+    /// **WARNING** is not used to check when objects are near
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("HasLanded");
+        int CollLayer = collision.gameObject.layer;
+        if(CollLayer==LayerMask.GetMask("Wall") || CollLayer==LayerMask.GetMask("CollidableObject")){
+            lastMove.Kill();
+        }
     }
 
     /// <summary>
     ///    Returns the max position where the player can jump, counting obstacles
     /// </summary>
     /// <returns>The new position</returns>
-    private Vector2 GetDestPos(){
+    private Vector2 GetDestPos(out float time){
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
 
-        //Restrict raycast to walls
-        LayerMask mask = LayerMask.GetMask("Wall");
+        //Restrict raycast to collidables first
+        LayerMask mask = LayerMask.GetMask("CollidableObject");
         RaycastHit2D hitPoint = Physics2D.Raycast(transform.position,direction, (dashDistance+radius),mask);
-
-        Debug.Log(hitPoint);
         if (hitPoint)
         {   
-            //TODO Modifier le calcul de l'endroit de stop pcq il faut être distant à la tangente
             Vector2 dest = hitPoint.point - (direction * cCollider.radius);
-            Debug.DrawRay(transform.position, direction * (Vector2.Distance(transform.position,dest)), Color.blue,0.5f);
+            float distance = Vector2.Distance(transform.position, dest);
+            time = distance / dashSpeed;
+            Debug.DrawRay(transform.position, direction * distance, Color.blue,0.5f);
+            return hitPoint.point - (direction * cCollider.radius);
+        }
+
+        //Restrict to wall
+        mask = LayerMask.GetMask("Wall");
+        hitPoint = Physics2D.Raycast(transform.position,direction, (dashDistance+radius),mask);
+        if (hitPoint)
+        {
+            ExtWall.Orientation wallOrt = hitPoint.collider.gameObject.GetComponent<ExtWall>().wallOrientation;
+            //TODO Terminer le calcul de la tangente si jamais on revient à DotWeen
+
+            Vector2 dest = hitPoint.point - (direction * cCollider.radius);
+            float distance = Vector2.Distance(transform.position, dest);
+            time = distance / dashSpeed;
+            Debug.DrawRay(transform.position, direction * distance, Color.blue,0.5f);
             return hitPoint.point - (direction * cCollider.radius);
         }
         else
         {
             Debug.DrawRay(transform.position, direction * 100, Color.yellow,0.5f);
+            time = dashDistance / dashSpeed;
             return (Vector2)transform.position + (direction * dashDistance);
         }
     }
@@ -66,10 +101,10 @@ public class Player : MonoBehaviour
     /// Moves the object to this position
     /// </summary>
     /// <param name="pos"></param>
-    private void MoveToPos(Vector2 pos){
+    private void MoveToPos(Vector2 pos,float time){
         //TODO fine tune la foction d'acceleration
         //TODO Changer le temps par une vitesse de déplacement (faire en sorte que le tps dépende de la distance)
-        transform.DOMove(pos,dashSpeed).SetEase(Ease.OutSine);
+        lastMove = transform.DOMove(pos,time).SetEase(Ease.OutQuart);
     }
 
 

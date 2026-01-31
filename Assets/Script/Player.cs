46cb34e4f983deefd39943e53ad4af187f8fa075
rdelpy <romain.delpy@u-bordeaux.fr>
2026-01-31 01:39:13 +0100

    Added jump dist & prefab

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float jumpDistance;
     Camera m_Camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -19,7 +20,11 @@ public class Player : MonoBehaviour
         {
             //Vector3 mousePosition = mouse.position.ReadValue();
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            MoveToPos(mousePosition);
+            Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
+
+            Debug.DrawRay(transform.position, direction * 100, Color.yellow);
+
+            //MoveToPos(mousePosition);
             // Ray ray = m_Camera.ScreenPointToRay(mousePosition);
             // if (Physics.Raycast(ray, out RaycastHit hit))
             // {
